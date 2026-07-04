161b75892adac5d0909311f9cbb95f37c9fe8ea7
Korneliusz Mietlicki <korneliuszm@gmail.com>
2026-07-04 23:46:16 +0200

    chore: bump version to 0.2.0

--- src/nd45_dtsu666/__init__.py
+++ src/nd45_dtsu666/__init__.py
@@ -1,3 +1,3 @@
 """Modbus bridge: Lumel ND45 (TCP) -> DTSU666 (RTU) for Sigenergy."""
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,4 +1,4 @@
 def test_package_imports():
     import nd45_dtsu666
 
-    assert nd45_dtsu666.__version__ == "0.1.0"
+    assert nd45_dtsu666.__version__ == "0.2.0"
